48a670bd76742cedbc1967f1f6ae70663b34f5fa
Amal Shamenov <130248846+ursinm@users.noreply.github.com>
2024-09-14 04:15:17 +0300

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,14 @@
 package com.epam.mjc;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
+    // Реализация метода divideBy, который возвращает Operation, делящий элементы списка на divider
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> list.stream()
+                           .map(element -> element / divider)  // Операция деления каждого элемента
+                           .collect(Collectors.toList());     // Собираем результат в список
     }
 }
